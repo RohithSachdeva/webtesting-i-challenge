026ce1de3bd481e03a3baeb4155345663f6565c1
RohithSachdeva <rsachdeva@ucdavis.edu>
2020-08-19 15:53:01 -0700

    Basis start

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -5,8 +5,18 @@ module.exports = {
   get,
 };
 
+// - a `success(item)` method that accepts an `item` object and **returns a new item** object modified according to the rules defined by the client for enhancement success. 
+//So enhancement will be a manual value inserted into the tests?  
+//If level is 20 .. not changed... so either < 20 or !== 20?  
+
+
+
 function success(item) {
-  return { ...item };
+  if (item.enhancement < 20) {
+  return { ...item, enhancement: item.enhancement + 1 };
+} else if (item.enhancement === 20) {
+  return(item)
+}
 }
 
 function fail(item) {
