6fe4860e9e94b2837d74234bb943ce3b9c742b81
RohithSachdeva <rsachdeva@ucdavis.edu>
2020-08-20 13:37:06 -0700

    5 tests

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -13,19 +13,40 @@ module.exports = {
 
 function success(item) {
   if (item.enhancement < 20) {
-  return { ...item, enhancement: item.enhancement + 1 };
+  return item.enhancement + 1 ;
 } else if (item.enhancement === 20) {
-  return(item)
+  return item.enhancement = 20;
 }
 }
 
+
+// - If the item's enhancement is less than 15, the durability of the item is decreased by 5.
+// - If the item's enhancement is 15 or more, the durability of the item is decreased by 10.
+// - If the item's enhancement level is greater than 16, the enhancement level decreases by 1 (17 goes down to 16, 18 goes down to 17).
+
 function fail(item) {
-  return { ...item };
+  if (item.enhancement < 15) {
+    return item.durability - 5;
+  }  else if (item.enhancement === 15) {
+    item.durability = item.durability - 10;
+    return{...item};
+  } else if (item.enhancement > 16) {
+    item.durability = item.durability - 10;
+    item.enhancement = item.enhancement - 1;
+    return { ...item };
+  }
+
 }
 
 function repair(item) {
-  return { ...item };
+  let repairedItem = {
+    ...item,
+    durability: 100,
+  };
+  return repairedItem;
 }
+
+// `item` object and **returns a new item** with the durability restored to 100.
 
 function get(item) {
   return { ...item };
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,88 @@
 const enhancer = require('./enhancer.js');
+
+
+
+
 // test away!
+
+//test success, fail, repair .... insert values for the items?  
+//durability: 85
+//enhancement: 33  ??   or enhancement = 33?  item.enhancement. 
+
+//     //success -> - The item's enhancement increases by 1.
+// - If the item enhancement level is 20, the enhancement level is not changed.
+// - The durability of the item is not changed.
+
+
+describe("Enhancer Tests", () => {
+    describe("success", () => {
+        it("Enhancement increases by 1, durability unchanged", () => {
+            let item = {
+                name: "destroyer",
+                durability: 20,
+                enhancement: 19
+            };
+            expect(enhancer.success(item)).toBe(20);
+        });
+    });
+    describe("repair", () => {
+        it("Repair durability to 100", () => {
+            let item = {
+                name: "Red scimitar",
+                durability: 45,
+                enhancement: 10,
+            };
+            expect(enhancer.repair(item)).toMatchObject({
+                durability: 100,
+            })
+        })
+    })
+    describe("fail", () => {
+        it("durability decreases if enhancement is less than 15", () => {
+            let item = {
+                name: "Big Kahuna",
+                durability: 50,
+                enhancement: 5,
+            };
+            expect(enhancer.fail(item)).toBe(45);
+        })
+    });
+
+    describe("fail2", () => {
+        it("durability decrease by 10 if enh is over 15", () => {
+            let item = {
+                name: "Little legend",
+                durability: 50,
+                enhancement: 15,
+            };
+            expect(enhancer.fail(item)).toMatchObject({
+                durability: 40,
+            });
+        })
+    })
+    
+    describe("fail3", () => {
+        it("durability decrease by 10 if enh is over 15", () => {
+            let item = {
+                name: "Little legend2",
+                durability: 50,
+                enhancement: 19,
+            };
+            expect(enhancer.fail(item)).toMatchObject({
+                durability: 40,
+                enhancement: 18,
+            });
+        })
+    })
+});
+
+
+
+
+
+
+
+
+// - If the item's enhancement is less than 15, the durability of the item is decreased by 5.
+// - If the item's enhancement is 15 or more, the durability of the item is decreased by 10.
+// - If the item's enhancement level is greater than 16, the enhancement level decreases by 1 (17 goes down to 16, 18 goes down to 17).
